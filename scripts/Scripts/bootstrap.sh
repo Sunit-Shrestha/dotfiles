35571005c71b4ba0a36dc2d3269df90168a9bf14
Sunit Shrestha <sunitshrsth@gmail.com>
2026-01-21 18:29:20 +0545

    Backup packages

--- scripts/Scripts/bootstrap.sh
+++ scripts/Scripts/bootstrap.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Download Arch packages
-sudo pacman -S --needed - < pacmanlist.txt
-yay -S --needed - < aurlist.txt
+# Enable multilib in /etc/pacman.conf first
+sudo pacman -S --needed - < ~/.dotfiles/scripts/Scripts/pacmanlist.txt
+
+# Yay and AUR Setup
+cd ~
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+yay -S --needed - < ~/.dotfiles/scripts/Scripts/aurlist.txt
 
 # Create base python virtual environment
 uv venv --python 3.12.10 ~/.uv/base/
@@ -10,14 +17,17 @@ source ~/.uv/base/bin/activate
 uv pip install -r requirements.txt
 deactivate
 
-# Yazi plugins
-ya pkg add dedukun/relative-motions
-ya pkg add yazi-rs/plugins:jump-to-char
+# Kanata Setup
+sudo groupadd uinput
+sudo usermod -aG uinput $USER
+mkdir -p ~/.config/systemd/user/
+cp ~/.config/kanata/kanata.service ~/.config/systemd/user/
+systemctl --user enable --now kanata.service
 
 # KDE Themes
 plasma-apply-colorscheme Dracula
 plasma-apply-cursortheme Breeze_Light
 plasma-apply-desktoptheme default
 papirus-folders --color bluegrey
-kwriteconfig5 --file kdeglobals --group Icons --key Theme Papirus
-kquitapp5 plasmashell && kstart5 plasmashell
+kwriteconfig6 --file kdeglobals --group Icons --key Theme Papirus
+kquitapp6 plasmashell && kstart plasmashell
